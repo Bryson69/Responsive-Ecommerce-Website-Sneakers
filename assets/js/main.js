b7fb16f5f50d5b452c5abb61bca36db3f846e672
Bryson69 <brysonmundia@gmail.com>
2020-11-15 22:47:27 +0300

    Correct indentation

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,23 +1,23 @@
-/*===== MENU SHOW =====*/ 
-const showMenu = (toggleId, navId)=>{
+/*===== MENU SHOW =====*/
+const showMenu = (toggleId, navId) => {
     const toggle = document.getElementById(toggleId),
-    nav = document.getElementById(navId)
+        nav = document.getElementById(navId)
 
-    if(toggle && nav){
-        toggle.addEventListener('click', ()=>{
+    if (toggle && nav) {
+        toggle.addEventListener('click', () => {
             nav.classList.toggle('show')
         })
     }
 }
 
-showMenu('nav-toggle','nav-menu')
+showMenu('nav-toggle', 'nav-menu')
 
 /*===== REMOVE MENU =====*/
 
 const navLink = document.querySelectorAll('.nav__link'),
     navMenu = document.getElementById('nav-menu')
 
-function linkAction(){
+function linkAction() {
     navMenu.classList.remove('show')
 }
 
@@ -29,26 +29,26 @@ const sections = document.querySelectorAll('section[id]')
 
 window.addEventListener('scroll', scrollActive)
 
-function scrollActive (){
+function scrollActive() {
     const scrollY = window.pageYOffset
 
-    sections.forEach(current=>{
+    sections.forEach(current => {
         const sectionHeight = current.offsetHeight
         const sectionTop = current.OffsetTop - 50
         sectionId = current.getAttribute('id')
 
-        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
-        document.querySelector('.nav__menu a[href*='+ sectionId +']').classList.add('active')
-        }else{
+        if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active')
+        } else {
             document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active')
         }
-        
+
     })
 }
+/*===== CHANGE COLOR HEADER =====*/
 
-/*===== CHANGE COLOR HEADER =====*/ 
-
-window.onscroll = ()=>{
-    const nav = document .getElementById('header')
-    if(this.scrollY >=200) nav.classList.add('scroll-header'); else nav.classList.remove('scroll-header')
+window.onscroll = () => {
+    const nav = document.getElementById('header')
+    if (this.scrollY >= 200) nav.classList.add('scroll-header');
+    else nav.classList.remove('scroll-header')
 }
